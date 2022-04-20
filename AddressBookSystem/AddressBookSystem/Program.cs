3fb0a595bf4a1fab86d5c9f13bbe2e5982db1f3f
Praan26 <arpanap102@gmail.com>
2022-04-20 19:03:37 +0530

    [Arpan] Add: Adding and Displaying new contact added

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using AddressBookSystem;
-Contact addressBook = new Contact();
-Console.WriteLine("Enter the First Name: ");
-addressBook.FirstName = Console.ReadLine();
-Console.WriteLine("Enter the Last Name: ");
-addressBook.LastName = Console.ReadLine();
-Console.WriteLine("Enter the Address: ");
-addressBook.Address = Console.ReadLine();
-Console.WriteLine("Enter the City: ");
-addressBook.City = Console.ReadLine();
-Console.WriteLine("Enter the State: ");
-addressBook.State = Console.ReadLine();
-Console.WriteLine("Enter the Zip Code: ");
-addressBook.ZipCode = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter the Phone Number: ");
-addressBook.PhoneNumber = Convert.ToInt64(Console.ReadLine());
-Console.WriteLine("Enter the EmailId: ");
-addressBook.EmailId = Console.ReadLine();
-Console.WriteLine("The Contact details are: " + addressBook.FirstName + " " + addressBook.LastName + " " + addressBook.Address + " " + addressBook.City + " " + addressBook.State + " " + addressBook.ZipCode+" "+addressBook.PhoneNumber+" "+addressBook.EmailId);
+public class Program
+{
+    public static void Main(String[] args )
+    {
+        AddressBook addressBook = new AddressBook();
+        bool end = true;
+        Console.WriteLine("1.Add new contact");
+        while ( end )
+        {
+            Console.WriteLine("Take an option to execute");
+            int option = Convert.ToInt32( Console.ReadLine());
+            switch( option )
+            {
+                case 1:
+                    Console.WriteLine("Enter the details: 1.First Name 2.Last Name 3.Address 4.City 5.State 6.Zip Code 7.Phone number 8.EmailId");
+                    addressBook.Display();
+                    break;
+                    case 2: 
+                    default: end = false;
+                    break;
+            }
+        }
+    }
+}
